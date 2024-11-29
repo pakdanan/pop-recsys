738acd8ecf128db6b516f0abd1c5a666fa25f772
PakDanan <pdanan@gmail.com>
2024-11-29 16:33:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,10 +33,6 @@ sorted_movies = load_data()
 # Extract unique genres
 all_genres = sorted_movies['genres'].str.split('|').explode().unique()
 
-# Streamlit UI
-st.title("Popular Movies Recommender System")
-st.markdown("( Based on IMDb Weighted Rating )")
-
 # Sidebar filters
 st.sidebar.header("Filters")
 selected_genres = st.sidebar.multiselect("Select Genres", all_genres, default=all_genres)
@@ -59,7 +55,7 @@ filtered_movies = sorted_movies[
 ].head(top_n)
 
 # Display movies in grid format
-st.subheader(f"Top {top_n} Movies")
+st.subheader(f"Top {top_n} Popular Recommendation Movies ( Based on IMDb Weighted Rating )")
 cols_per_row = 3  # Number of columns per row
 rows = [filtered_movies.iloc[i:i + cols_per_row] for i in range(0, len(filtered_movies), cols_per_row)]
 
